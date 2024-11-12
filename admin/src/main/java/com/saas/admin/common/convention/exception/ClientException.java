1d82db68c8ed468508d60fef91a6c8330cadf929
yibai <404223879@qq.com>
2024-11-12 23:19:40 +0800

    客户端异常

---
+++ admin/src/main/java/com/saas/admin/common/convention/exception/ClientException.java
@@ -0,0 +1,33 @@
+package com.saas.admin.common.convention.exception;
+
+import com.saas.admin.common.convention.errorcode.BaseErrorCode;
+import com.saas.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * 客户端异常
+ */
+public class ClientException extends AbstractException {
+    public ClientException(IErrorCode errorCode) {
+        this(errorCode, null, null);
+    }
+
+    public ClientException(String message) {
+        this(BaseErrorCode.CLIENT_ERROR, message, null);
+    }
+
+    public ClientException(IErrorCode errorCode, String message) {
+        this(errorCode,message,  null);
+    }
+
+    public ClientException(IErrorCode errorCode, String message, Throwable throwable) {
+        super(message, errorCode, throwable);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientException{" +
+                "errorCode='" + errorCode + '\'' +
+                ", errorMessage='" + errorMessage + '\'' +
+                '}';
+    }
+}
